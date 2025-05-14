35629a8239f9db10a7f19448965bb80d86ce9f04
sakutena <210246600+sakutena@users.noreply.github.com>
2025-05-14 23:32:18 +0900

    road images is fileloader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,21 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test:/\.(png|jpg)/,
+                use:[
+                    {
+                    loader:'file-loader',
+                    options:{
+                        esModule:false,
+                        name:'images/[name].[ext]',
+
+                    }
+
+                }
+
+                ]
+            }
         ],
     },
     plugins:[
